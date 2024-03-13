38fdabb7568bb41d236eec6b74261f6fbf301c0d
Dimitris Chios <chiosdimit@gmail.com>
2024-03-13 18:37:50 +0200

    add an id

--- src/index.js
+++ src/index.js
@@ -3,40 +3,40 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const firstBook = {
-  author: "Ms. Rachel",
-  title:
-    "Ms. Rachel and the Special Surprise: Encouraging Speech and Learning Through Play and Music",
-  img: "./images/book1.jpg",
-};
-const secondBook = {
-  author: "Kristin Hannah",
-  title: "The Women: A Novel",
-  img: "./images/book2.jpg",
-};
+const books = [
+  {
+    author: "Ms. Rachel",
+    title:
+      "Ms. Rachel and the Special Surprise: Encouraging Speech and Learning Through Play and Music",
+    img: "./images/book1.jpg",
+    id: 1,
+  },
+  {
+    author: "Kristin Hannah",
+    title: "The Women: A Novel",
+    img: "./images/book2.jpg",
+    id: 2,
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
+
+
 const Book = (props) => {
+  const { img, title, author} = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
