9229724602dc0b499056d0b826ce36d34d59cf65
Dimitris Chios <chiosdimit@gmail.com>
2024-03-23 12:26:00 +0200

    create the books file array

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+
+export const books = [
+  {
+    author: "Ms. Rachel",
+    title:
+      "Ms. Rachel and the Special Surprise: Encouraging Speech and Learning Through Play and Music",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Kristin Hannah",
+    title: "The Women: A Novel",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Frank Herbert",
+    title: "Dune (Dune Chronicles, Book 1)",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,22 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-
-const books = [
-  {
-    author: "Ms. Rachel",
-    title:
-      "Ms. Rachel and the Special Surprise: Encouraging Speech and Learning Through Play and Music",
-    img: "./images/book1.jpg",
-    id: 1,
-  },
-  {
-    author: "Kristin Hannah",
-    title: "The Women: A Novel",
-    img: "./images/book2.jpg",
-    id: 2,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
@@ -26,18 +12,6 @@ const BookList = () => {
         return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-
-const Book = (props) => {
-  const { img, title, author} = props;
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
